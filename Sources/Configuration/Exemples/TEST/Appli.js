3652fad13f62432a0dc06b25cb52cf16e98330ed
amandine-sahl <amandine.sahl@gmail.com>
2015-01-21 15:38:04 +0100

    Ajout de fichier exemple de configuration (WMTS IGN) fixed #2

---
+++ Sources/Configuration/Exemples/TEST/Appli.js
@@ -0,0 +1,25 @@
+//Paramétrage d'installation
+// répertoire de stockage des fichiers GPX
+const CST_cheminRelatifGPX = '../../GPX/PNX/';
+// répertoire de stockage des photos
+const CST_cheminRelatifPhoto = '../../Photos/PNX/';
+// choix du mode d'affichage
+const CST_choixEspeceForcee = false;
+// choix du mode d'affichage
+const CST_choixModeAffichage = true;
+// titre de l'application
+const CST_titreAppli = 'Observations occasionnelles naturalistes des Parcs nationaux de France'; // idem que "Serveur.php"
+// URL de contact sinon null
+const CST_urlContact = 'http://www.parcsnationaux.fr/Acces-direct/Nous-contacter';
+
+//Configuration navigateur
+const CST_appli = 'ObsOccPNX'; // nom unique idem que "PostGreSQL.php"
+
+//Configuration du formulaire de saisie des observations occasionelles
+// propres aux métadonnées de la base sinon null
+const CST_id_structure = null;
+const CST_id_etude = null;
+const CST_id_protocole = null;
+
+// Activation du cryptage RSA  (désactivé pour le site de démo)
+const CST_Cryptage = true; // idem que "PostGreSQL.php"
---
+++ Sources/Configuration/Exemples/TEST/PostGreSQL.php
@@ -0,0 +1,18 @@
+<?php
+    //Paramètres fixes de connexion à la base PostGreSQL
+    define('HOST', 'localhost');
+    define('PORT', '5432');
+    define('DBNAME', 'obs_occ_pnx');
+    define('USER', DBNAME . '_cnx');
+    define('PASSWORD', DBNAME . '_cnx');
+    define('BASENAME', DBNAME); //Préfixe des rôles de la base
+    //Configuration navigateur
+    define('APPLI', 'ObsOccPNX'); // nom unique idem que dans "Appli.js"
+
+    // Activation du cryptage RSA (désactivé pour le site de démo)
+    define('CRYPTAGE', true); // nom unique idem que dans "Appli.js"
+
+    // Image de fond d'accueil
+    //$fond = '../Configuration/Exemples/PNX/SpiralePnX.jpg';
+    define('FOND', getimagesize($fond) ? 'url(' . $fond . ')' : 'none');
+?>
