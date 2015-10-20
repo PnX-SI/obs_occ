760baaac6e4a88051ca3999d2cce1c9478434e5c
amandine-sahl <amandine.sahl@gmail.com>
2015-10-20 19:13:34 +0200

    Chagement de la requête car son comportement était étrange (tri de 10000 données sur la date) PS : pense à rendre toutes les fonction IMMUTABLE

--- Sources/Modeles/GeoJson/gjObs.php
+++ Sources/Modeles/GeoJson/gjObs.php
@@ -18,35 +18,33 @@
     $sort = str_replace('lieu_dit', 'IGN_BD_TOPO.LIEU_DIT.nom', $sort);
 
     $req = "WITH tri AS (
-        SELECT row_number() over (order by " . $sort . ' ' . $dir . " NULLS LAST), id_obs  
-        FROM saisie.saisie_observation
+        SELECT row_number() over (order by " . $sort . ' ' . $dir . " NULLS LAST), obs.*,  
+            commune.nom AS commune, lieu_dit.nom AS lieu_dit,
+            (numerisateurs.nom || ' ' || numerisateurs.prenom)::varchar(250) AS numerisat, 
+            (validateurs.nom || ' ' || validateurs.prenom)::varchar(250) AS validat,
+            nom_etude,libelle, depart
+        FROM saisie.saisie_observation obs
         LEFT JOIN md.etude using(id_etude) left JOIN md.protocole USING(id_protocole) 
         LEFT JOIN md.personne AS numerisateurs ON numerisateur = numerisateurs.id_personne 
         LEFT JOIN md.personne AS validateurs ON validateur = validateurs.id_personne 
-        LEFT JOIN ign_bd_topo.lieu_dit ON id_lieu_dit = ign_bd_topo.lieu_dit.id 
-        LEFT JOIN ign_bd_topo.commune ON ign_bd_topo.commune.code_insee = saisie.saisie_observation.code_insee
+        LEFT JOIN ign_bd_topo.lieu_dit ON id_lieu_dit = lieu_dit.id 
+        LEFT JOIN ign_bd_topo.commune ON commune.code_insee =obs.code_insee
         WHERE " . $where . ' AND ' . $and .' AND ' . $filter . ")
     SELECT 
-        st_asgeojson(st_transform(SAISIE.SAISIE_OBSERVATION.geometrie, 4326)), 
+        st_asgeojson(st_transform(geometrie, 4326)), 
         id_obs, heure_obs, date_obs, date_debut_obs, date_fin_obs, date_textuelle,
-        SAISIE_OBSERVATION.regne, SAISIE_OBSERVATION.nom_vern, SAISIE_OBSERVATION.nom_complet,
-        cd_nom, phylum, classe, ordre, famille, nom_valide, ST_GeometryType(SAISIE.SAISIE_OBSERVATION.geometrie),
+        regne, nom_vern, nom_complet,
+        cd_nom, phylum, classe, ordre, famille, nom_valide, ST_GeometryType(geometrie),
         effectif, effectif_min, effectif_max, effectif_textuel, type_effectif, phenologie,
-        precision, determination, id_waypoint, depart, latitude, elevation, IGN_BD_TOPO.COMMUNE.code_insee,
-        substring(IGN_BD_TOPO.COMMUNE.code_insee from 1 for 2) AS dep, IGN_BD_TOPO.COMMUNE.nom AS commune,
-        id_lieu_dit, IGN_BD_TOPO.LIEU_DIT.nom AS lieu_dit, observateur, md.liste_nom_auteur(observateur)
+        precision, determination, id_waypoint, depart, latitude, elevation, code_insee,
+        substring(code_insee from 1 for 2) AS dep, commune,
+        id_lieu_dit, lieu_dit, observateur, md.liste_nom_auteur(observateur)
         AS observat, validateur, structure, md.liste_nom_structure(structure) AS struct,
         localisation, id_etude, nom_etude, id_protocole, libelle,
         diffusable, remarque_obs, statut_validation, decision_validation, url_photo,
-        (NUMERISATEURS.nom || ' ' || NUMERISATEURS.prenom) AS numerisat, longitude,
-        commentaire_photo, (VALIDATEURS.nom || ' ' || VALIDATEURS.prenom) AS validat
-    FROM SAISIE.SAISIE_OBSERVATION JOIN tri USING(id_obs)
-    LEFT JOIN MD.ETUDE USING(id_etude) 
-    LEFT JOIN MD.PROTOCOLE USING(id_protocole) 
-    LEFT JOIN MD.PERSONNE AS NUMERISATEURS ON numerisateur = NUMERISATEURS.id_personne 
-    LEFT JOIN MD.PERSONNE AS VALIDATEURS ON validateur = VALIDATEURS.id_personne 
-    LEFT JOIN IGN_BD_TOPO.LIEU_DIT ON id_lieu_dit = IGN_BD_TOPO.LIEU_DIT.id 
-    LEFT JOIN IGN_BD_TOPO.COMMUNE ON IGN_BD_TOPO.COMMUNE.code_insee = SAISIE.SAISIE_OBSERVATION.code_insee
+        numerisat, longitude,
+        commentaire_photo, validat
+    FROM tri
     WHERE row_number > " . $start . ' LIMIT ' . $limit;
 
     $rs = $cnxPgObsOcc->executeSql($req);
